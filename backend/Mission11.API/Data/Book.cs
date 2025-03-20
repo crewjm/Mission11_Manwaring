edd73da2cdd614eeb54bbcd2c5f20352badcba1c
crewjm <crewjm@byu.edu>
2025-03-20 17:59:19 -0600

    Initial

---
+++ backend/Mission11.API/Data/Book.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Mission11.API.Data;
+
+public class Book
+{
+    [Key]
+    public int BookID { get; set; }
+    [Required]
+    public string Title { get; set; } = "";
+    [Required]
+    public string Author { get; set; } = "";
+    [Required]
+    public string Publisher { get; set; } = "";
+    [Required]
+    public string ISBN { get; set; } = "";
+    [Required]
+    public string Category { get; set; } = "";
+    [Required]
+    public int PageCount { get; set; } = 0;
+    [Required]
+    public decimal Price { get; set; } = 0;
+}
---
+++ backend/Mission11.API/Data/BookDbContext.cs
@@ -0,0 +1,14 @@
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mission11.API.Data;
+
+public class BookDbContext : DbContext 
+{
+    public BookDbContext(DbContextOptions<BookDbContext> options) : base(options) 
+    { 
+            
+    }
+
+    public DbSet<Book> Books { get; set; }
+}
